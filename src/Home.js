af0f748ea5b56c22d8e0d25917ba60db56f6e69c
nthnlbdky <nthnlwebdev@gmail.com>
2021-09-25 23:49:55 +0800

    added ppoduct props

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,13 @@ const Home = () => {
         <div className="home">
             <div className="home__container">
                 <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="" className="home__image" />
-            </div>
             <div className="home__row">
-                <Product/>
+                <Product 
+                title="The Lean Start up"
+                price={29.99}
+                image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+                rating={5}
+                />
                 <Product/>
             </div>
             <div className="home__row">
@@ -19,7 +23,7 @@ const Home = () => {
             <div className="home__row">
                 <Product/>  
             </div>
-            
+            </div>
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,22 @@
 import './Product.css'
 
-const Product = () => {
+const Product = ({title, image, price, rating}) => {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean StartUp</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>15.66</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
+                    {Array(rating).fill().map((_, i)=>(
+                        <p>✡</p>
+                    ))}
                     <p>✡</p>
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+            <img src={image} alt="" />
             <button>Add to Basket</button>
         </div>
     )
